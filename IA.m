0a476d993cc5cdb5a290c984cfae1a8ae9d10cf3
Nahuel <nahuel.arellano@gmail.com>
2017-02-19 18:39:39 -0300

    Se agrego la etapa de test al programa y se comenzo a redactar el informe.

--- IA.m
+++ IA.m
@@ -4,33 +4,33 @@ fprintf('INICIANDO...\n');
 %%% MATRIZ P[D1xD2]= P[5376x268]
 %%% D1 = 5376: 256Hz * 3.5 seg * 6CH
 %%% D2 =  268: Muestras
-load('P_TODO.mat');
+load('input/TRAIN.mat');
 
 %%% CARGA TARGET DEL CONJUNTO DE ENTRENAMIENTO
 %%% MATRIZ T[D1xD2] = T[1x268]
 %%%  D1 =   1: CLASE ( 1 o -1 ) 
 %%%  D2 = 268: MUESTRAS
-%%% - LOS PRIMEROS  135 REGISTROS SON DE LA CLASE  1
-%%% - LOS RESTANTES 133 REGISTROS SON DE LA CLASE -1
+%%% - LOS PRIMEROS  135 REGISTROS SON DE LA CLASE 0:  1
+%%% - LOS RESTANTES 133 REGISTROS SON DE LA CLASE 1: -1
 T = [ones(1,135) -ones(1,133)];
 Q = size(P,2); % Cantidad de muestras
 
 % VALORES INICIALES
-n1 = 5;  % Cantidad de neuronas capa oculta.
+%{
+n1 = 73;  % Cantidad de neuronas capa oculta.
 %%% INICIALIZACIÓN RANDOM DE LOS PESOS SINAPTICOS.
 ep = 1;   % factor de variacion ( -1 a 1 )
 W1 = 2*ep*rand(n1,size(P,1)) - ep;
 b1 = 2*ep*rand(n1,1) - ep;  
 W2 = 2*ep*rand(1,n1) - ep;
 b2 = 2*ep*rand - ep;
-%%%
-
+%}
 %%% INICIALIZACIÓN DE PESOS SINAPTICOS ALMACENADOS.
-%load('VARIABLES.mat'); % Pesos almacenados.
+load('var/VARIABLES.mat'); % Pesos almacenados con 73 neuronas
 alfa = 0.01; %Valor de ajuste
 
 fprintf('PROCESANDO...\n');
-for Epocas = 1:500
+for Epocas = 1:1000
     if mod(Epocas, 10) == 0
         fprintf('EPOCA: %d\n', Epocas);
     end
@@ -76,6 +76,38 @@ plot(a,'r*')
 plot([135, 135], [-1, 1],'b')
 hold off;
 
-fprintf('PORCENTAJE CLASIFICADOS CORRECTAMENTE %3.2f %%\n', (CantidadOk * 100 / 268));
-fprintf('PORCENTAJE CLASIFICADOS ERRONEAMENTE  %3.2f %%\n', (CantidadError * 100 / 268));
+fprintf('---------- CONJUNTO ENTRENAMIENTO ------------\n');
+fprintf('PORCENTAJE CLASIFICADOS CORRECTAMENTE %3.2f %%\n', (CantidadOk * 100 / Q));
+fprintf('PORCENTAJE CLASIFICADOS ERRONEAMENTE  %3.2f %%\n', (CantidadError * 100 / Q));
+
+
+%%% CARGA CONJUNTO DE TEST
+%%% MATRIZ P[D1xD2]= P[5376x293]
+%%% D1 = 5376: 256Hz * 3.5 seg * 6CH
+%%% D2 =  293: Muestras
+load('input/TEST.mat');
+
+%%% SE CARGA LA ETIQUETAS CON EL VALOR CORRESPONDIENTE PARA EL CONJUNTO
+%%% DE TEST
+load('input/LABELS_TEST.mat');
+Q2 = size(TEST,2);           % Cantidad de muestras
+
+% Verificación de la respuesta.
+CantidadOkTest = 0;
+CantidadErrorTest = 0;
+for q = 1:Q2
+    %%% Calculo el resultado del conjunto TEST en mi RNA-MLP entrenada
+    RESULT = tansig(W2*tansig(W1*TEST(:,q) + b1) + b2);
+    if( RESULT > 0) RESULTADO = 0; else RESULTADO = 1; end;
+    %%% Verifico el valor obtenido contra el real
+    if( (LABELS(q) - RESULTADO) == 0)
+         CantidadOkTest = CantidadOkTest + 1;
+    else
+         CantidadErrorTest = CantidadErrorTest + 1;
+    end
+end
+
+fprintf('-------------- CONJUNTO TEST -----------------\n');
+fprintf('PORCENTAJE CLASIFICADOS CORRECTAMENTE %3.2f %%\n', (CantidadOkTest * 100 / Q2));
+fprintf('PORCENTAJE CLASIFICADOS ERRONEAMENTE  %3.2f %%\n', (CantidadErrorTest * 100 / Q2));
 fprintf('FINALIZADO\n');
